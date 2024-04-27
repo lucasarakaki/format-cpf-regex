702daef4ba168f8fc14c80356301cd007cb48140
Lucas Takeshi <takeshioficial01@gmail.com>
2024-04-27 17:01:33 -0300

    Corrigindo a string do Exception

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     # Validando exceções
     try {
       if ($result == false) {
-        throw new Exception("digite 0s 11 dígitos do seu CPF sem pontos e traços");
+        throw new Exception("digite os 11 dígitos do seu CPF sem pontos e traços");
       }
       # Formantando o CPF com o preg_replace
       if ($result) {
